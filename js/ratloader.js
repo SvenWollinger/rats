f4347340dbf4e9b12fd5ecc049cf2d39efc74b41
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2023-09-25 11:07:44 +0200

    Do the ratter

---
+++ js/ratloader.js
@@ -0,0 +1,18 @@
+function loadRat(rat, onSuccess) {
+    get("/json/rats/" + rat + ".json", (json) => {
+        let rat = JSON.parse(json)
+        onSuccess(rat)
+    })
+}
+
+function get(url, onSuccess) {
+    let request = new XMLHttpRequest()
+    request.open("GET", url)
+    request.send()
+    request.onreadystatechange = (e) => {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if(request.status === 200)
+                onSuccess(request.responseText)
+        }
+    }
+}
